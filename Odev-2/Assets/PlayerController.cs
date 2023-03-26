8e3287b7f6f0d543ea32cd5009605fb30719693b
Hamza Albaş <hamza.albas@hotmail.com>
2023-03-26 11:56:33 +0300

    4 yönlü hereket.

--- Odev-2/Assets/PlayerController.cs
+++ Odev-2/Assets/PlayerController.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 5;
-    private float moveX;
+    private float moveX, moveY;
 
 
     private void Update()
     {
         moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
 
         transform.Translate(movementSpeed * moveX * Time.deltaTime * Vector3.right);
+        transform.Translate(movementSpeed * moveY * Time.deltaTime * Vector3.forward);
         
     }
 }
